449a35bb606aba6317800a0c3db33f9a280c4d56
rociovenus <45718613+rociovenus@users.noreply.github.com>
2020-10-07 19:43:00 -0400

    Update question_type.py

--- quizc/model/question_type.py
+++ quizc/model/question_type.py
@@ -1,4 +1,4 @@
-import datetime
+ import datetime
 from enum import Enum
 from quizc.model.validations import ValidatorType
 
@@ -28,8 +28,14 @@ class DateConfiguration(QuestionConfiguration):
             return datetime.datetime.strptime(value, DateConfiguration.DATE_FORMAT)
         except ValueError:
             return None
-
-
+class NumericConfiguration(QuestionConfiguration):
+ 
+    def __init__(self):
+        QuestionConfiguration.__init__(self, False, [ValidatorType.REQUIRED, ValidatorType.MIN])
+    def convert_value(self, value):
+        value=int(value)
+        
+        return value
 class PickOneQuestionConfiguration(QuestionConfiguration):
     def __init__(self):
         QuestionConfiguration.__init__(self, True, [ValidatorType.REQUIRED])
@@ -38,7 +44,8 @@ class PickOneQuestionConfiguration(QuestionConfiguration):
 class QuestionType(Enum):
     TEXT = (1, TextConfiguration())
     DATE = (2, DateConfiguration())
-    PICK_ONE = (3, PickOneQuestionConfiguration())
+    NUMERIC = (3,NumericConfiguration())
+    PICK_ONE = (4, PickOneQuestionConfiguration())
 
     def __init__(self, code, configuration):
         self.code = code
@@ -59,3 +66,4 @@ class QuestionType(Enum):
             if question_type.code == code or question_type.code == int(code):
                 return question_type
         return None
+
